183df90b45d427582b03c2b0d4d4d1d0fab20bc3
KranthiTeja59 <96741610+KranthiTeja59@users.noreply.github.com>
2022-09-10 11:40:35 +0530

    import java.util.Scanner; class Code {     public static void main(String args[])     {         char n;         Scanner sc=new Scanner(System.in);         n=sc.nextLine().charAt(0);         if(n=='v' | n=='V')         {             System.out.println("Violet");         }         else if(n=='i' | n=='I')         {             System.out.println("Indigo");         }         else if(n=='b' | n=='B')         {             System.out.println("Blue");         }         else if(n=='g' | n=='G')         {             System.out.println("Green");         }         else if(n=='y' | n=='Y')         {             System.out.println("Yellow");         }         else if(n=='o' | n=='O')         {             System.out.println("Orange");         }         else if(n=='r' | n=='R')         {             System.out.println("Red");         }         else         {             System.out.println(-1);         }         sc.close();     } }

---
+++ Color_Code.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+class Code
+{
+    public static void main(String args[])
+    {
+        char n;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextLine().charAt(0);
+        if(n=='v' | n=='V')
+        {
+            System.out.println("Violet");
+        }
+        else if(n=='i' | n=='I')
+        {
+            System.out.println("Indigo");
+        }
+        else if(n=='b' | n=='B')
+        {
+            System.out.println("Blue");
+        }
+        else if(n=='g' | n=='G')
+        {
+            System.out.println("Green");
+        }
+        else if(n=='y' | n=='Y')
+        {
+            System.out.println("Yellow");
+        }
+        else if(n=='o' | n=='O')
+        {
+            System.out.println("Orange");
+        }
+        else if(n=='r' | n=='R')
+        {
+            System.out.println("Red");
+        }
+        else
+        {
+            System.out.println(-1);
+        }
+        sc.close();
+    }
+}
