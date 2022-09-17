cfb49dbc1d5c74fe1c6a6692bbe7ccad3a90a216
KranthiTeja59 <96741610+KranthiTeja59@users.noreply.github.com>
2022-09-17 11:50:59 +0530

    import java.util.Scanner; class Pat {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,j;         n=sc.nextInt();         for(i=1;i<=n;i++)         {             for(j=1;j<=n;j++)             {                 if(i>=j)                 {                     System.out.print(j);                 }             }             System.out.print(" ");         }
        }
    }

--- print_the_following_pattern.java
+++ print_the_following_pattern.java
@@ -4,29 +4,20 @@ class Pat
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int N,i,j;
-        N=sc.nextInt();
-        for(i=1;i<=N;i++)
+        int n,i,j;
+        n=sc.nextInt();
+        for(i=1;i<=n;i++)
         {
-            for(j=1;j<=N;j++)
+            for(j=1;j<=n;j++)
             {
-                if(i==j | i+j==N+1)
+                if(i>=j)
                 {
-                    System.out.print("x");
-                }
-                else
-                {
-                    System.out.print("0");
+                    System.out.print(j);
                 }
             }
             System.out.print("
 ");
         }
-        sc.close();
+        
     }
 }
-//11 12 13 14 15
-//21 22 23 24 25
-//31 32 33 34 35
-//41 42 43 44 45
-//51 52 53 54 55
