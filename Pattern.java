f203623c5e5ccd9942efa9f5c024f67ffab4d703
KranthiTeja59 <96741610+KranthiTeja59@users.noreply.github.com>
2022-09-17 15:31:31 +0530

    import java.util.Scanner; class Pat {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,i,j;         n=sc.nextInt();         for(i=1;i<=n;i++)         {             for(j=1;j<=n;j++)             {                 System.out.print(i);                 System.out.print(" ");             }             System.out.print(" ");         }     } }

---
+++ Pattern.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Pat
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,i,j;
+        n=sc.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=n;j++)
+            {
+                System.out.print(i);
+                System.out.print(" ");
+            }
+            System.out.print("
+");
+        }
+    }
+}
