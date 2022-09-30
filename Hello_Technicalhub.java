8138caa53713e00756cda7e3f7b1b9b3e0b7220f
KranthiTeja59 <96741610+KranthiTeja59@users.noreply.github.com>
2022-09-30 15:22:59 +0530

    import java.util.Scanner; class Hlo {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String s1="Hello Technicalhub";         String s2=sc.nextLine();         System.out.println(s1);         System.out.println(s2);
        }
    }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Hlo
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String s1="Hello Technicalhub";
+        String s2=sc.nextLine();
+        System.out.println(s1);
+        System.out.println(s2);
+        
+        
+    }
+}
