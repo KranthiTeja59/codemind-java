0e212e6805c039d276c3d4eb8d8f99f5fd34dbcb
KranthiTeja59 <96741610+KranthiTeja59@users.noreply.github.com>
2022-10-11 00:16:17 +0530

    import java.util.Scanner; class Pat {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String str=sc.nextLine();         String res=str.toLowerCase();         System.out.print(res);     } }

---
+++ To_Lower_Case.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Pat
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        String res=str.toLowerCase();
+        System.out.print(res);
+    }
+}
