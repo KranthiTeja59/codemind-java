0e74ee958633398fe791a023bf54e5aafee5fdee
KranthiTeja59 <96741610+KranthiTeja59@users.noreply.github.com>
2022-10-25 22:46:18 +0530

    import java.util.Scanner; class Pat {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int N,i,c=0,s=0,av;         N=sc.nextInt();         int arr[]=new int[N];         for(i=0;i<N;i++)         {             arr[i]=sc.nextInt();         }         for(i=0;i<N;i++)         {             s+=arr[i];         }         av=s/N;         for(i=0;i<N;i++)         {             if(arr[i]>=av)             {                 c+=1;             }         }         System.out.println(c);     } }

---
+++ elements_greater_than_average.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Pat
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int N,i,c=0,s=0,av;
+        N=sc.nextInt();
+        int arr[]=new int[N];
+        for(i=0;i<N;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        for(i=0;i<N;i++)
+        {
+            s+=arr[i];
+        }
+        av=s/N;
+        for(i=0;i<N;i++)
+        {
+            if(arr[i]>=av)
+            {
+                c+=1;
+            }
+        }
+        System.out.println(c);
+    }
+}
